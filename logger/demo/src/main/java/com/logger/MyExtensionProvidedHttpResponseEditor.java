f9b570579193c6811b492d4b20c0e5dfc45754c3
beishanxueyuan <138347114+beishanxueyuan@users.noreply.github.com>
2024-04-27 20:21:59 +0800

    Update MyExtensionProvidedHttpResponseEditor.java

--- logger/demo/src/main/java/com/logger/MyExtensionProvidedHttpResponseEditor.java
+++ logger/demo/src/main/java/com/logger/MyExtensionProvidedHttpResponseEditor.java
@@ -1,35 +1,60 @@
+/*
+* Copyright (c) 2023. PortSwigger Ltd. All rights reserved.
+*
+* This code may be used to extend the functionality of Burp Suite Community Edition
+* and Burp Suite Professional, provided that this usage does not violate the
+* license terms for those products.
+*/
+
 package com.logger;
 
 import burp.api.montoya.MontoyaApi;
 import burp.api.montoya.core.ByteArray;
 import burp.api.montoya.http.message.HttpRequestResponse;
-import burp.api.montoya.http.message.responses.HttpResponse;
+import burp.api.montoya.http.message.requests.HttpRequest;
 import burp.api.montoya.logging.Logging;
 import burp.api.montoya.ui.Selection;
+import burp.api.montoya.ui.editor.EditorOptions;
 import burp.api.montoya.ui.editor.RawEditor;
 import burp.api.montoya.ui.editor.extension.EditorCreationContext;
-import burp.api.montoya.ui.editor.extension.ExtensionProvidedHttpResponseEditor;
+import burp.api.montoya.ui.editor.extension.EditorMode;
+import burp.api.montoya.ui.editor.extension.ExtensionProvidedHttpRequestEditor;
 import static burp.api.montoya.core.ByteArray.byteArray;
 import java.awt.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.nio.charset.StandardCharsets;
-
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.databind.SerializationFeature;
 
-class MyExtensionProvidedHttpResponseEditor implements ExtensionProvidedHttpResponseEditor {
-    private final RawEditor ResponseEditor;
+
+
+class MyExtensionProvidedHttpRequestEditor implements ExtensionProvidedHttpRequestEditor {
+    private final RawEditor requestEditor;
 
     private HttpRequestResponse requestResponse;
     private final MontoyaApi api;
 
-    MyExtensionProvidedHttpResponseEditor(MontoyaApi api, EditorCreationContext creationContext) {
+    MyExtensionProvidedHttpRequestEditor(MontoyaApi api, EditorCreationContext creationContext) {
         this.api = api;
-        ResponseEditor = api.userInterface().createRawEditor();
+        if (creationContext.editorMode() == EditorMode.READ_ONLY) {
+            requestEditor = api.userInterface().createRawEditor(EditorOptions.READ_ONLY);
+        } else {
+            requestEditor = api.userInterface().createRawEditor();
+        }
     }
 
-    
+    @Override
+    public boolean isEnabledFor(HttpRequestResponse requestResponse) {
+        if (requestResponse.request().bodyToString() != null && requestResponse.request().bodyToString() != "") {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
 
     @Override
     public String caption() {
@@ -38,43 +63,39 @@ class MyExtensionProvidedHttpResponseEditor implements ExtensionProvidedHttpResp
 
     @Override
     public Component uiComponent() {
-        return ResponseEditor.uiComponent();
+        return requestEditor.uiComponent();
     }
     
 
     @Override
     public Selection selectedData() {
-        return ResponseEditor.selection().isPresent() ? ResponseEditor.selection().get() : null;
+        return requestEditor.selection().isPresent() ? requestEditor.selection().get() : null;
     }
 
     @Override
     public boolean isModified() {
-        return ResponseEditor.isModified();
+        return requestEditor.isModified();
     }
 
     @Override
-    public HttpResponse getResponse() {
-        HttpResponse Response;
-        Response = requestResponse.response();
-        return Response;
-    }
-    @Override
-    public boolean isEnabledFor(HttpRequestResponse requestResponse) {
-        if (requestResponse.response().bodyToString() != null && requestResponse.response().bodyToString() != "") {
-            return true;
+    public HttpRequest getRequest() {
+        HttpRequest request;
+        if (requestEditor.isModified()) {
+            request = requestResponse.request().withBody(requestEditor.getContents());
         } else {
-            return false;
+            request = requestResponse.request();
         }
-
+        return request;
     }
+    
     @Override
     public void setRequestResponse(HttpRequestResponse requestResponse) {
         this.requestResponse = requestResponse;
-        String body_str = requestResponse.response().bodyToString();
+        String body_str = requestResponse.request().bodyToString();
         String beautifiedJson = "";
         Logging logging = api.logging();
         ByteArray body;
-        if ((body_str.startsWith("{\""))) {
+        if ((body_str.contains("\":\""))) {
             ObjectMapper objectMapper = new ObjectMapper();
             ObjectWriter writer = objectMapper.writer().with(SerializationFeature.INDENT_OUTPUT);
             Object json;
@@ -84,22 +105,12 @@ class MyExtensionProvidedHttpResponseEditor implements ExtensionProvidedHttpResp
             } catch (JsonProcessingException e) {
                 e.printStackTrace();
             }
-            api.logging().logToOutput(body_str);
-            if(body_str.matches(".*[\\\\u4e00-\\\\u9fa5].*")){
-                api.logging().logToOutput("111");
-                body = byteArray(beautifiedJson.getBytes(StandardCharsets.UTF_8));
-            }
-            else{
-                api.logging().logToOutput("222");
-                body = byteArray(beautifiedJson);
-            }
-        }
-        else {
+            body = byteArray(beautifiedJson);
+        } else {
             body_str = body_str.replace("&", "\n&");
             body = byteArray(body_str);
         }
-        this.ResponseEditor.setContents(body);
+        this.requestEditor.setContents(body);
     }
-    
-    
 }
+
